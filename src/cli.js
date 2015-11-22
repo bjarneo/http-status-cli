3947e80f76caa7782c66c8fc88eb22a724982942
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2015-11-22 15:56:30 +0100

    change variable name

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var message = require('./status-codes');
+var statusCodes = require('./status-codes');
 
 if (!process.argv[2]) {
     return false;
 }
 
-var code = message[process.argv[2]];
+var code = statusCodes[process.argv[2]];
 if (code) {
     console.log('%d - %s \n\r%s', process.argv[2], code.message, code.description);
 } else {
