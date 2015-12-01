5f7ad9edd9d30654b6d938900d8edd4bcda33037
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2015-12-01 21:18:34 +0100

    lint the test

--- test/index.test.js
+++ test/index.test.js
@@ -5,10 +5,15 @@ var httpStatus = require('../src/index');
 
 describe('http status', function() {
     it('should return status and description if the code is valid', function() {
-        httpStatus(418, function(err, msg, desc) {
+        httpStatus(200, function(err, msg, desc) {
             assert.equal(err, null);
-            assert.equal(msg, 'I\'m a teapot');
-            assert.equal(desc, 'This code was defined in 1998 as one of the traditional IETF April Fools\' jokes, in RFC 2324, Hyper Text Coffee Pot Control Protocol, and is not expected to be implemented by actual HTTP servers. The RFC specifies this code should be returned by tea pots requested to brew coffee. This HTTP status is used as an easter egg in some websites, including Google.com.');
+            assert.equal(msg, 'OK');
+            assert.equal(
+                desc,
+                'The 200 status code is by far the most common returned.' +
+                ' It means, simply, that the request was received and' +
+                ' understood and is being processed.'
+            );
         });
     });
 
