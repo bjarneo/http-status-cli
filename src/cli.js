bdaf6a4b524339d640ae75a4b8fe8c8fe3a20a7f
Brad Harris <bmharris@gmail.com>
2016-01-10 18:46:46 -0700

    printing all status codes if none was provided

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var httpStatus = require('./index');
+var codes = require('./status-codes');
 
+// Print all status codes if none are provided
 if (!process.argv[2]) {
-    return false;
+    Object.keys(codes).forEach(function(code) {
+        printCode(code, codes[code].message, codes[code].description);
+    });
+    return;
 }
 
 httpStatus(process.argv[2], function(err, msg, desc) {
@@ -11,6 +16,10 @@ httpStatus(process.argv[2], function(err, msg, desc) {
         console.log(err);
     }
 
-    console.log('%d - %s', process.argv[2], msg);
-    console.log(desc);
+    printCode(process.argv[2], msg, desc);
 });
+
+function printCode(code, msg, desc) {
+    console.log('%d - %s', code, msg);
+    console.log(desc);
+}
