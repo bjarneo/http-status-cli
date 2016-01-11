7c12b4ca0694bd0fca5d18a7538cd71efb70fb03
Brad Harris <bmharris@gmail.com>
2016-01-10 18:51:03 -0700

    adding colors to output

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('colors');
 var httpStatus = require('./index');
 var codes = require('./status-codes');
 
@@ -13,13 +14,21 @@ if (!process.argv[2]) {
 
 httpStatus(process.argv[2], function(err, msg, desc) {
     if (err) {
-        console.log(err);
+        console.log(err.red);
+        return;
     }
 
     printCode(process.argv[2], msg, desc);
 });
 
 function printCode(code, msg, desc) {
-    console.log('%d - %s', code, msg);
-    console.log(desc);
+    if(code < 400) {
+        code = code.green
+    }else if(code < 500) {
+        code = code.yellow
+    }else {
+        code = code.red
+    }
+    console.log('%s - %s', code, msg.gray);
+    console.log('  %s', desc);
 }
